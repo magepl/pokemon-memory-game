688f8b16e4b45c56eca0100ea9c915a9819a0eeb
magepl <marketlinkresearch@gmail.com>
2024-03-20 20:51:10 +0000

    add ease in/out transition for cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,7 +2,7 @@ import pokeball from '../../assets/Poke Ball.webp';
 
 export default function Card() {
   return (
-    <div className="border-2 flex rounded-xl h-40 w-32 hover:h-44 hover:w-36">
+    <div className="border-2 flex rounded-xl h-40 w-32 transition ease-linear delay-150 hover:h-44 hover:w-36">
       <img
         className="object-contain content-center justify-center"
         src={pokeball}
